cbe7970632299bd696d4f2139e50e2bd1f26adcb
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2024-02-03 18:41:50 +0530

    handle api unavailable

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,17 +1,27 @@
 import axios from "axios"
 
 const getWeather = async ({lat, lng}, signal) => {
-  const { data } = await axios.get(
-    "https://api.open-meteo.com/v1/forecast?latitude="+lat+
-  "&longitude="+lng+"&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m",
-  {
-    signal,
-  });
-
-  const result = {
-    temperature: `${data.current.temperature_2m}${data.current_units.temperature_2m}`,
-    windspeed: `${data.current.wind_speed_10m}${data.current_units.wind_speed_10m}`
+  let result = {
+    temperature: "Unavailable",
+    windspeed: "Unavailable",
   }
+
+  try {
+    const { data } = await axios.get(
+      "https://api.open-meteo.com/v1/forecast?latitude="+lat+
+    "&longitude="+lng+"&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m",
+    {
+      signal,
+    });
+
+    result = {
+      temperature: `${data.current.temperature_2m}${data.current_units.temperature_2m}`,
+      windspeed: `${data.current.wind_speed_10m}${data.current_units.wind_speed_10m}`
+    }
+  } catch (e) {
+    console.log("Error fetching weather info")
+  }
+
 
   return result;
 }
